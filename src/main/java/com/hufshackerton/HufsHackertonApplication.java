b59fe21994607aad53b14c20c92af307b42ab9a0
jorippppong <duelee75@gmail.com>
2024-06-22 19:47:06 +0900

    first commit

---
+++ src/main/java/com/hufshackerton/HufsHackertonApplication.java
@@ -0,0 +1,13 @@
+package com.hufshackerton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HufsHackertonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HufsHackertonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hufshackerton/HufsHackertonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufshackerton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HufsHackertonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
