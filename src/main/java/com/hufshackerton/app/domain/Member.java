0b48f53f18951a1f9d1d7666e7a6f574ea2e4c25
jorippppong <duelee75@gmail.com>
2024-06-25 23:40:15 +0900

    member 엔티티 추가

---
+++ src/main/java/com/hufshackerton/app/domain/Member.java
@@ -0,0 +1,24 @@
+package com.hufshackerton.app.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String profileImageUrl;
+}
